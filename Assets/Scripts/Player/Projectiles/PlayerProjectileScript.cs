68561fafda8fbae489472823d552ad6cbbde7575
CrunchyBoi <ananas@hotmail.com>
2023-02-17 15:33:02 +0300

    Working on basic projectile system #5

--- Assets/Scripts/Player/Projectiles/PlayerProjectileScript.cs
+++ Assets/Scripts/Player/Projectiles/PlayerProjectileScript.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerProjectileScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject basicProjectile;
+    private GameObject projectileClone;
+    private GameObject weaponRotationOrigin;
+    private Vector3 initialProjectilePosition;
+    private Quaternion initialProjectileRotation;
+    private bool isFiring = false;
+    private float fireTimer = 0f;
+    private float fireRate = 3f;
+    private void Start()
     {
-        
+        if(GameObject.FindGameObjectWithTag("WeaponRotationOrigin") != null)
+        {
+            weaponRotationOrigin = GameObject.FindGameObjectWithTag("WeaponRotationOrigin");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireProjectile();
+            isFiring = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            fireTimer = 0f;
+            isFiring= false;
+        }
+
+        if(isFiring)
+        {
+            fireTimer += Time.deltaTime;
+            if(fireTimer > (1f / fireRate))
+            {
+                fireTimer = 0f;
+                FireProjectile();
+            }
+        }
+
+        initialProjectilePosition = new Vector3(weaponRotationOrigin.transform.GetChild(1).gameObject.transform.position.x + 0.5f,
+                                                weaponRotationOrigin.transform.GetChild(1).gameObject.transform.position.y + 0.15f,
+                                                weaponRotationOrigin.transform.GetChild(1).gameObject.transform.position.z);
+        initialProjectileRotation = Quaternion.Euler(0f, 0f, weaponRotationOrigin.transform.rotation.z);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FireProjectile()
     {
-        
+        //Rotation is the rotation of the weapon rotation origins rotation z and 0-0 for x and y
+        //Initial position is 2.5f plus on the x and 0.75 on the y axis for the weapon position
+        projectileClone = Instantiate(basicProjectile, initialProjectilePosition, initialProjectileRotation);
     }
 }
