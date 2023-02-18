1ecbebb8c886854713ccf567adcdc2e05acf15f5
CrunchyBoi <ananas@hotmail.com>
2023-02-18 09:09:51 +0300

    Basic ammo and reload logic complete

--- Assets/Scripts/Player/Projectiles/PlayerProjectileScript.cs
+++ Assets/Scripts/Player/Projectiles/PlayerProjectileScript.cs
@@ -9,17 +9,24 @@ public class PlayerProjectileScript : MonoBehaviour
     [SerializeField] private float fireRate = 3f;
     [SerializeField] private float accuracy = 1f;
     [SerializeField] private float projectileLifetime = 2f;
+    [SerializeField] private float maxAmmo = 20f;
+    [SerializeField] private float reloadDuration = 2f;
+    private float currentAmmo = 20f;
     private GameObject projectileClone;
     private Quaternion initialProjectileRotation;
     private Vector3 projectileSpawnPos;
     private float fireTimer = 0f;
     private bool isFiring = false;
+    private bool isReloading = false;
     
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            FireProjectile();
+            if(!isReloading)
+            {
+                fireProjectile();
+            }
             isFiring = true;
         }
         else if (Input.GetMouseButtonUp(0))
@@ -28,18 +35,23 @@ public class PlayerProjectileScript : MonoBehaviour
             isFiring= false;
         }
 
-        if(isFiring)
+        if(Input.GetKeyDown(KeyCode.R) && !isReloading && currentAmmo < maxAmmo)
+        {
+            StartCoroutine(reload());
+        }
+
+        if(isFiring && !isReloading)
         {
             fireTimer += Time.deltaTime;
             if(fireTimer > (1f / fireRate))
             {
                 fireTimer = 0f;
-                FireProjectile();
+                fireProjectile();
             }
         }     
     }
 
-    private void CalculateProjectilePositionAndRotation()
+    private void calculateProjectilePositionAndRotation()
     {
         if (GameObject.FindGameObjectWithTag("projectileSpawn"))
         {
@@ -53,11 +65,26 @@ public class PlayerProjectileScript : MonoBehaviour
         yield return new WaitForSeconds(projectileLifetime);
         Destroy(projectile);
     }
-    private void FireProjectile()
+    private IEnumerator reload()
     {
-        CalculateProjectilePositionAndRotation();
+        isReloading = true;
+        yield return new WaitForSeconds(reloadDuration);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+    private void fireProjectile()
+    {
+        calculateProjectilePositionAndRotation();
         projectileClone = Instantiate(basicProjectile, projectileSpawnPos, initialProjectileRotation);
         projectileClone.GetComponent<Rigidbody2D>().AddForce((Vector2)(initialProjectileRotation * Vector2.right) * projectileForce);
         StartCoroutine(projectileLifeTime(projectileClone));
+        currentAmmo--;
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(reload());
+        }
     }
 }
+//Ammo / Reload logic
+//VFX - Camera shake, recoil animation, muzzle flash, reload slider /animation?
+//SFX - Fire sfx, empty sfx, reload sfx
