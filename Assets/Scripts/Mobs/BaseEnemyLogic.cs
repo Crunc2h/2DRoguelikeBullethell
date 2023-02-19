f288a20cc8d60df7b511ac9fe7038dd75858c815
CrunchyBoi <ananas@hotmail.com>
2023-02-19 14:47:45 +0300

    Bugfix for enemy AI

--- Assets/Scripts/Mobs/BaseEnemyLogic.cs
+++ Assets/Scripts/Mobs/BaseEnemyLogic.cs
@@ -5,65 +5,67 @@ using Pathfinding;
 
 public class BaseEnemyLogic : MonoBehaviour
 {
-    //Aims at the player
-    //Walks towards the player, but uses pathfinding to move around obstacles
-    //Stops when it is within a certain distance and starts shooting (only if it has clear line of sight, it keeps coming closer if it doesn't)
 
+
+    [Header("Raycast Variables")]
+    [SerializeField] private float raycastRange = 10f;
     private RaycastHit2D hitPlayer;
     private RaycastHit2D hitObstacle;
     private LayerMask playerLayer;
     private LayerMask obstacleLayer;
     private Vector2 weaponDirection;
-    private float raycastDistance = 10f;
-
-    private float dirnChangeDur = 0.5f;
+    [Header("Movement Variables")]
+    [SerializeField] private float movementSpeed = 5f;    
     private float dirChangeTimer = 0f;
-    private float movementSpeed = 8f;
+    private float distanceToPlayer;
+    private float dirChangeDur;
     private Rigidbody2D rb;
     private Vector2 movementDirection;
     private bool clearLineOfSight = false;
     private bool manualMovement = false;
+    private bool shiftOnCooldown = false;
     private void Awake()
     {
         obstacleLayer = LayerMask.GetMask("Obstacle");
         playerLayer = LayerMask.GetMask("PlayerLayer");
-
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         enemySight();
-
-        dirChangeTimer += Time.deltaTime;
-        if(dirChangeTimer >= dirnChangeDur)
+        AIvsManualMovementControlManager();
+    }
+    private void FixedUpdate()
+    {
+        if(manualMovement)
         {
-            dirChangeTimer = 0f;
-            dirnChangeDur = Random.Range(0.5f, 1.5f);
-            CalculateMovementDirection();
+            MovementDirectionChangeManager();
+            Move();
         }
-        if (((Vector2)transform.position - (Vector2)GameObject.FindGameObjectWithTag("Player").transform.position).magnitude < 10f && clearLineOfSight)
+    }
+    private void AIvsManualMovementControlManager()
+    {
+        distanceToPlayer = (transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude;
+        if (distanceToPlayer < 10f && clearLineOfSight && !manualMovement)
         {
             manualMovement = true;
             gameObject.GetComponent<SimpleSmoothModifier>().enabled = false;
             gameObject.GetComponent<AIPath>().enabled = false;
             gameObject.GetComponent<AIDestinationSetter>().enabled = false;
         }
-        else
+        else if(!clearLineOfSight || distanceToPlayer > 10f)
         {
-            StartCoroutine(gearShiftCooldown());
+            if(manualMovement && !shiftOnCooldown)
+            {
+                StartCoroutine(MovementShiftCooldown());
+            }
         }
     }
-    private void FixedUpdate()
+    private IEnumerator MovementShiftCooldown()
     {
-        if(manualMovement)
-        {
-            Move();
-        }
-    }
-
-    private IEnumerator gearShiftCooldown()
-    {
+        shiftOnCooldown = true;
         yield return new WaitForSeconds(1f);
+        shiftOnCooldown = false;
         manualMovement = false;
         gameObject.GetComponent<AIDestinationSetter>().enabled = true;
         gameObject.GetComponent<AIPath>().enabled = true;
@@ -72,34 +74,67 @@ public class BaseEnemyLogic : MonoBehaviour
     private void enemySight()
     {
         weaponDirection = GetComponent<BaseAimFunctionality>().weaponAimDirection;
-        hitPlayer = Physics2D.Raycast((Vector2)transform.position, weaponDirection, raycastDistance, playerLayer);
-        if (hitPlayer.collider != null)
-        {
-            Debug.DrawLine(transform.position, hitPlayer.point, Color.red);
-        }
-        else
-        {
-            Debug.DrawLine(transform.position, ((Vector2)transform.position + weaponDirection * raycastDistance), Color.green);
-        }
-        hitObstacle = Physics2D.Raycast((Vector2)transform.position, weaponDirection, raycastDistance, obstacleLayer);
-        if (hitObstacle.collider != null)
-        {
-            Debug.DrawLine(transform.position, hitObstacle.point, Color.red);
-        }
-        else
-        {
-            Debug.DrawLine(transform.position, ((Vector2)transform.position + weaponDirection * raycastDistance), Color.green);
-        }
-
-        if(hitPlayer.collider != null && hitObstacle.collider == null)
+        
+        hitPlayer = Physics2D.Raycast((Vector2)transform.position, weaponDirection, raycastRange, playerLayer);
+        hitObstacle = Physics2D.Raycast((Vector2)transform.position, weaponDirection, raycastRange, obstacleLayer);
+        
+        if (hitPlayer.collider != null && hitObstacle.collider == null)
         {
             GetComponentInChildren<BaseWeaponFunctionalityEnemy>().fireCommand = true;
             clearLineOfSight = true;
         }
         else
         {
-            GetComponentInChildren<BaseWeaponFunctionalityEnemy>().fireCommand = false;
-            clearLineOfSight = false;
+            if (hitObstacle.collider != null && hitPlayer.collider != null)
+            {
+                if (((Vector2)transform.position - hitObstacle.point).magnitude > ((Vector2)transform.position - hitPlayer.point).magnitude)
+                {
+                    GetComponentInChildren<BaseWeaponFunctionalityEnemy>().fireCommand = true;
+                    clearLineOfSight = true;
+                }
+                else
+                {
+                    GetComponentInChildren<BaseWeaponFunctionalityEnemy>().fireCommand = false;
+                    clearLineOfSight = false;
+                }
+            }
+            else
+            {
+                GetComponentInChildren<BaseWeaponFunctionalityEnemy>().fireCommand = false;
+                clearLineOfSight = false;
+            }
+        }
+
+
+        
+        //DEBUG
+        if (hitPlayer.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitPlayer.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, ((Vector2)transform.position + weaponDirection * raycastRange), Color.green);
+        }        
+        //DEBUG
+        if (hitObstacle.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitObstacle.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, ((Vector2)transform.position + weaponDirection * raycastRange), Color.green);
+        }
+        
+    }
+    private void MovementDirectionChangeManager()
+    {
+        dirChangeTimer += Time.fixedDeltaTime;
+        if (dirChangeTimer >= dirChangeDur)
+        {
+            dirChangeTimer = 0f;
+            dirChangeDur = Random.Range(0.5f, 2f);
+            CalculateMovementDirection();
         }
     }
     private void CalculateMovementDirection()
