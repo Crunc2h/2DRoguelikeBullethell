8b903d8891a0d88055ee5523759b516d2fd52143
CrunchyBoi <ananas@hotmail.com>
2023-02-19 10:31:34 +0300

    Working on basic enemy logic #4

--- Assets/Scripts/Weapons/BaseWeaponFunctionalityEnemy.cs
+++ Assets/Scripts/Weapons/BaseWeaponFunctionalityEnemy.cs
@@ -10,30 +10,39 @@ public class BaseWeaponFunctionalityEnemy : MonoBehaviour
     [SerializeField] private float projectileLifetime = 2f;
     [SerializeField] private float fireRate = 3f;
     [SerializeField] private float accuracy = 1f;
-    [SerializeField] private float maxAmmo = 20f;
-    [SerializeField] private float currentAmmo = 20f;
-    [SerializeField] private float reloadDuration = 2f;
+
     [Header("SFX")]
     [SerializeField] private AudioSource pistolFireSFX;
     private GameObject projectileClone;
+    private GameObject muzzleFlash;
+    private GameObject bulletTrail;
     private Quaternion initialProjectileRotation;
     private Vector3 projectileSpawnPos;
-    public bool fireCommand = false;
+    private bool stopFiring = true;
 
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            stopFiring = !stopFiring;
             StartCoroutine(automaticFiring());
+        }
+        if (transform.GetChild(2).gameObject.CompareTag("muzzleFlash"))
+        {
+            muzzleFlash = transform.GetChild(2).gameObject;
+        }
+        if (transform.GetChild(3).gameObject.CompareTag("bulletTrail"))
+        {
+            bulletTrail = transform.GetChild(3).gameObject;
         }
     }
     private IEnumerator automaticFiring()
     {
-        while (fireCommand)
+        while (!stopFiring)
         {
             fireProjectileLogic();
-            yield return new WaitForSeconds(1 / fireRate);
+            yield return new WaitForSeconds((1 / fireRate) + Random.Range(-0.5f * (1 / fireRate), 0.5f * (1 / fireRate)));
         }
     }
     private void fireProjectileLogic()
@@ -46,7 +55,7 @@ public class BaseWeaponFunctionalityEnemy : MonoBehaviour
     }
     private void calculateProjectilePositionAndRotation()
     {
-        if (gameObject.transform.GetChild(1).CompareTag("prjectileSpawnPoint"))
+        if (gameObject.transform.GetChild(1).CompareTag("projectileSpawnPoint"))
         {
             projectileSpawnPos = transform.GetChild(1).gameObject.transform.position;
         }
@@ -62,20 +71,21 @@ public class BaseWeaponFunctionalityEnemy : MonoBehaviour
     {
         pistolFireSFX.Play();
         GetComponent<Animator>().SetTrigger("recoil");
-        StartCoroutine(muzzleFlash());
-        StartCoroutine(bulletTrail());
+        StartCoroutine(muzzleFlashFX());
+        StartCoroutine(bulletTrailFX());
     }
 
-    private IEnumerator muzzleFlash()
+    private IEnumerator muzzleFlashFX()
     {
-        GameObject.FindGameObjectWithTag("muzzleFlash").GetComponent<SpriteRenderer>().enabled = true;
+        muzzleFlash.GetComponent<SpriteRenderer>().enabled = true;
         yield return new WaitForSeconds(0.1f);
-        GameObject.FindGameObjectWithTag("muzzleFlash").GetComponent<SpriteRenderer>().enabled = false;
+        muzzleFlash.GetComponent<SpriteRenderer>().enabled = false;
     }
-    private IEnumerator bulletTrail()
+    private IEnumerator bulletTrailFX()
     {
-        GameObject.FindGameObjectWithTag("bulletTrail").GetComponent<SpriteRenderer>().enabled = true;
+        bulletTrail.GetComponent<SpriteRenderer>().enabled = true;
         yield return new WaitForSeconds(0.05f);
-        GameObject.FindGameObjectWithTag("bulletTrail").GetComponent<SpriteRenderer>().enabled = false;
+        bulletTrail.GetComponent<SpriteRenderer>().enabled = false;
     }
 }
+
